63adaf26a1676a9495bb365da78f27e13450f80b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-29 07:31:09 +0100

    yeå

--- build/CMakeFiles/myexecutable.dir/src/main.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/myexecutable.dir/src/main.c.o: \
- /home/tom/projects/c_project/src/main.c /usr/include/stdc-predef.h \
- /home/tom/projects/c_project/src/hello/hello.h
---
+++ run.bat
@@ -0,0 +1,7 @@
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cd bin
+MyProjectC.exe
+cd ../../
