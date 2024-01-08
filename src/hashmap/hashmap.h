c763409efea860a1d84a84d389aa34bca940976e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-01-08 03:10:26 +0100

    m

--- src/hashmap/hashmap.h
+++ src/hashmap/hashmap.h
@@ -1,20 +1,5 @@
 // #pragma once
 
-<<<<<<< HEAD
-// #include <assert.h>
-// #include <stddef.h> // for size_t
-// #include <stdint.h>
-// #include <stdlib.h> // for malloc, free
-// #include <string.h> // for strdup, strlen
-
-// typedef struct HashTable HashTable;
-
-// void hashmap_insert(HashTable **table, const char *key, int value);
-// int hashmap_remove(HashTable *table, const char *key);
-// int hashmap_get(const HashTable *table, const char *key);
-// void hashmap_init(HashTable **table, uint32_t max_size, size_t element_size);
-// void hashmap_destroy(HashTable *table);
-=======
 #include <assert.h>
 #include <stddef.h> // for size_t
 #include <stdint.h>
@@ -35,7 +20,6 @@ int hashmap_remove(HashTable *table, const char *key);
 int hashmap_get(const HashTable *table, const char *key, int *found);
 void hashmap_init(HashTable *table, uint32_t max_size, size_t element_size);
 void hashmap_destroy(HashTable *table);
->>>>>>> 9acac7b0ddef2112aea730d1094ac0275ef62a08
 
 // // for benchmarking
 // uint32_t xxHash32(const void *input, size_t length, uint32_t seed);
